b4e3e72fc7da7b17cf7164ea487fa1286868aabe
Manu Farfaro <manufarfaro@gmail.com>
2013-09-13 18:02:39 -0300

    Dummy commit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,8 @@
 class BootStrap {
 
     def init = { servletContext ->
+		println "-----------------------"
+		println System.env.DATABASE_URL
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,5 @@
 dataSource {
     pooled = true
-	uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-	username = uri.userInfo.split(":")[0]
-	password = uri.userInfo.split(":")[1]
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -31,8 +28,10 @@ environments {
 			dbCreate = "update"
 			driverClassName = "org.postgresql.Driver"
 			dialect = org.hibernate.dialect.PostgreSQLDialect
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 			url = "jdbc:postgresql://"+uri.host+uri.path
-			
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
 		}
 	}
 }
