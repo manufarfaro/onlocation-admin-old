52e604b2c1f9a75a98052a9cbc172e99ab6d1b10
Manu Farfaro <manufarfaro@gmail.com>
2013-10-07 00:08:29 -0300

    #3: WIP. Se avanza con Tests.

--- test/unit/com/onlocation/index/IndexControllerTests.groovy
+++ test/unit/com/onlocation/index/IndexControllerTests.groovy
@@ -1,7 +1,5 @@
 package com.onlocation.index
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 
@@ -9,9 +7,14 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(IndexController)
-class IndexControllerTests {
-
-    void testSomething() {
-       fail "Implement me"
+class IndexControllerTests{
+	
+	IndexController controller = new IndexController()
+	
+    void testIndexMapping() {
+		
+       controller.index()
+	   
+	   assertForwardUrlMapping("/",action: "index")
     }
 }
