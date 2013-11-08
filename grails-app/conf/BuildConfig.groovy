d5be95c899fdbbbc45e81c7c1a8a8b11e5ce7572
Manu Farfaro <manufarfaro@gmail.com>
2013-11-08 03:03:15 -0300

    #8: BuildConfig Test enviroment added.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,7 +21,7 @@ grails.project.dependency.resolution = {
 				// specify dependency exclusions here; for example, uncomment this to disable ehcache:
 				// excludes 'ehcache'
 			}
-			log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+			log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 			checksums true // Whether to verify checksums on resolve
 			legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
@@ -55,9 +55,55 @@ grails.project.dependency.resolution = {
 				runtime ":database-migration:1.3.2"
 
 				compile ':cache:1.0.1'
+
 				test ":code-coverage:1.2.6"
 			}
 		}
+		test{
+			// inherit Grails' default dependencies
+			inherits("global") {
+				// specify dependency exclusions here; for example, uncomment this to disable ehcache:
+				// excludes 'ehcache'
+			}
+			log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+			checksums true // Whether to verify checksums on resolve
+			legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+			repositories {
+				inherits true // Whether to inherit repository definitions from plugins
+
+				grailsPlugins()
+				grailsHome()
+				grailsCentral()
+
+				mavenLocal()
+				mavenCentral()
+
+				// uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+				//mavenRepo "http://snapshots.repository.codehaus.org"
+				//mavenRepo "http://repository.codehaus.org"
+				//mavenRepo "http://download.java.net/maven/2/"
+				//mavenRepo "http://repository.jboss.com/maven2/"
+			}
+
+			dependencies { // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+				runtime 'postgresql:postgresql:8.4-702.jdbc3' }
+
+			plugins {
+				runtime ":hibernate:$grailsVersion"
+				runtime ":jquery:1.8.3"
+				runtime ":resources:1.2"
+
+				build ':jetty:2.0.3'
+
+				runtime ":database-migration:1.3.2"
+
+				compile ':cache:1.0.1'
+
+				test ":code-coverage:1.2.6"
+			}
+		}
+
 		production{
 			// inherit Grails' default dependencies
 			inherits("global") {
