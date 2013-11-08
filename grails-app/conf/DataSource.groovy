f5c646fda541bfe6b0cd88be89fdfc01cdb7f3ce
Manu Farfaro <manufarfaro@gmail.com>
2013-11-08 03:33:25 -0300

    #8: Added TRAVIS env to Datasource.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,13 +17,19 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "create-drop"
-			driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-			 uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-			url = "jdbc:postgresql://"+uri.host+uri.path
-			username = uri.userInfo.split(":")[0]
-			password = uri.userInfo.split(":")[1]
+			if(System.env.TRAVIS){
+				dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+				driverClassName = "org.h2.Driver"
+				url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			} else {
+				dbCreate = "create-drop"
+				driverClassName = "org.postgresql.Driver"
+				dialect = org.hibernate.dialect.PostgreSQLDialect
+				uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+				url = "jdbc:postgresql://"+uri.host+uri.path
+				username = uri.userInfo.split(":")[0]
+				password = uri.userInfo.split(":")[1]
+			}
         }
     }
 	production {
